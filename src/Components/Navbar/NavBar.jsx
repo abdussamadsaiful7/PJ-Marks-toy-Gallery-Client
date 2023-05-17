e98dd067d7a9e5838ebe6b7d653c8940c6dade46
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-05-18 00:39:21 +0600

    added blog component

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -11,14 +11,14 @@ const NavBar = () => {
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 font-bold shadow bg-base-100 rounded-box w-52">
                             <li><Link to='/'>Home</Link></li>
                             <li tabIndex={0}>
                                 <a className="justify-between">
                                     Toys
                                     <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
                                 </a>
-                                <ul className="p-2">
+                                <ul className="p-2 text-green-500">
                                     <li><a>All Toys</a></li>
                                     <li><a>Add Toy</a></li>
                                     <li><a>My Toy</a></li>
@@ -33,7 +33,7 @@ const NavBar = () => {
                         FAN<span className='text-green-600'>tasy-gall</span>ERY</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
+                    <ul className="menu menu-horizontal px-1 text-green-500 font-bold">
                         <li><Link to='/'>Home</Link></li>
                         <li><Link>All Toys</Link></li>
                         <li><Link>Add Toy</Link></li>
@@ -43,7 +43,7 @@ const NavBar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <Link><a className="lg:btn lg:btn-outline btn-warning hidden">About Us</a></Link>
+                    <Link><a className="lg:btn lg:btn-outline btn-success hidden">About Us</a></Link>
                 </div>
             </div>
         </div>
